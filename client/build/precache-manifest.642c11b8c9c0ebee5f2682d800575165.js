6124d16d6397dd93bfa94cbac7009ac9c509dfcb
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-24 14:24:04 -0500

    updating routes

---
+++ client/build/precache-manifest.642c11b8c9c0ebee5f2682d800575165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b697ef4d6733203fc5fa99b8df9e997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ea7ccd853e513fa489",
+    "url": "/static/css/main.8f3e99a8.chunk.css"
+  },
+  {
+    "revision": "b6866b3721d2dce05958",
+    "url": "/static/js/2.d97f49d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d97f49d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ea7ccd853e513fa489",
+    "url": "/static/js/main.ded27341.chunk.js"
+  },
+  {
+    "revision": "228efc25977fc7b53c58",
+    "url": "/static/js/runtime-main.105206bc.js"
+  }
+]);
