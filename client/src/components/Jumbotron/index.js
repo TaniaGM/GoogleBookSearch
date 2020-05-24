16c3da0cde84f229039cf7775bbccc0bdc7d489f
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-24 13:34:53 -0500

    trying to fix  broken codes

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>MyBookKeeper</h1>
+            <p>Find your next great book!</p>
+        </div>
+    );
+}
+
+export default Jumbotron;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import "./style.css";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-dark bg-black">
+            <a className="navbar-brand" href="/">
+                MBK
+      </a>
+        </nav>
+    );
+};
+
+export default Nav;
