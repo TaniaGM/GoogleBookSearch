3392ea6fc0cb224bfc8e90524f342ae23f32f8e6
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-24 20:14:29 -0500

    updating routes

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const bookRoutes = require("./books");
-
-// creating all possible api/ routes to be exported in this index file
-router.use("/books", bookRoutes)
-
-module.exports = router;
-
-// localhost:3001/api/books
